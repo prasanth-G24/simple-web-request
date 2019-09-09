6bd23f1ae08e431a7791e84734985e2175a3e4e3
prasanth-G24 <35618065+prasanth-G24@users.noreply.github.com>
2019-09-09 18:10:55 +0530

    deleting old files

--- readmail.py
+++
@@ -1,9 +0,0 @@
-'''import imaplib
-username="prasanthmaverick@gmail.com"
-password="printf(\"123\");"
-mail=imaplib.IMAP4_SSL("imap.gmail.com")
-mail.login(username,password)
-mail.select("INBOX")
-result,data=mail.uid('search',None,'ALL')'''
-a='%06d'%(1)
-print(a)
